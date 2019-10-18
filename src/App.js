afcf4da5ea1a1276ea20a436de0a0a45ec863d31
MuellerMarvin <marvin.mueller@code.berlin>
2019-10-18 19:10:38 +0200

    Implement basic function and design

--- src/App.js
+++ src/App.js
@@ -2,23 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [ 'apples', 'bananas', 'lemons' ],
+    };
+  }
+
+  render() {
+    var listItems = this.state.list.map((item, i) => <ListItem itemName={ item } index={ i }></ListItem>)
+    return (
+      <div className="App">
+        <h1>Public Shoppinglist</h1>
+        <AddDialog/>
+        <li>
+          { listItems }
+        </li>
+      </div>
+    );
+  }
+}
+
+class AddDialog extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="AddDialog">
+        <input id="addDialogTextField" type="text"/>
+        <button>Add</button>
+      </div>
+    );
+  }
+}
+
+function ListItem(props) {
+  return(
+    <div className="ListItem">
+      <li>
+        { props.itemName }
+      </li>
+      <button>x</button>
     </div>
   );
 }

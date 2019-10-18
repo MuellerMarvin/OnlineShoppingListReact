95694f9fe55511d374c14e2a689e719ed27cc762
MuellerMarvin <marvin.mueller@code.berlin>
2019-10-18 19:12:37 +0200

    Implement all basic functionality and design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import deleteIcon from './delete-24px.svg'
 import './App.css';
 
 class App extends React.Component {
@@ -7,19 +8,42 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      list: [ 'apples', 'bananas', 'lemons' ],
+      list: [ 'apples', 'bananas', 'cucumber', 'coconut', 'orange', 'pineapple', 'lemons' ],
     };
+
+    this.AddItem = this.AddItem.bind(this);
+    this.RemoveItem = this.RemoveItem.bind(this);
+  }
+
+  AddItem(itemName) {
+    var newList = this.state.list.slice();
+    newList.push(itemName);
+    this.setState({ list: newList });
+  }
+
+  RemoveItem(index) {
+    var newList = this.state.list.slice();
+    newList.splice(index, 1);
+    this.setState({ list: newList });
   }
 
   render() {
-    var listItems = this.state.list.map((item, i) => <ListItem itemName={ item } index={ i }></ListItem>)
+    var listDisplay;
+
+    if (this.state.list.length > 0) {
+      listDisplay = this.state.list.map((item, i) => <ListItem itemName={ item } onAddButtonClick={ this.AddItem } onRemoveButtonClick={ this.RemoveItem } /> )
+    }
+    else {
+      listDisplay = <p class="noselect">There is no items to show!</p>
+    }
+
     return (
       <div className="App">
         <h1>Public Shoppinglist</h1>
-        <AddDialog/>
-        <li>
-          { listItems }
-        </li>
+        <AddDialog onAddButtonClick={ this.AddItem }/>
+        <div className="ListWrapper">
+          { listDisplay }
+        </div>
       </div>
     );
   }
@@ -30,11 +54,25 @@ class AddDialog extends React.Component {
     super(props);
   }
 
+  runAddEvent() {
+    // retrieve and clear the value of the textfield
+    var value = document.getElementById('AddDialogTextField').value;
+    document.getElementById('AddDialogTextField').value = "";
+
+    this.props.onAddButtonClick(value);
+  }
+
+  checkForEnter = (event) => {
+    if(event.key === 'Enter') {
+      this.runAddEvent();
+    }
+  }
+
   render() {
     return (
       <div className="AddDialog">
-        <input id="addDialogTextField" type="text"/>
-        <button>Add</button>
+        <input id="AddDialogTextField" type="text" onKeyUp={ this.checkForEnter }/>
+        <button onClick={() => this.runAddEvent() }>Add</button>
       </div>
     );
   }
@@ -43,10 +81,10 @@ class AddDialog extends React.Component {
 function ListItem(props) {
   return(
     <div className="ListItem">
-      <li>
+      <p>
         { props.itemName }
-      </li>
-      <button>x</button>
+      </p>
+      <input class="ImageButton" type="image" src={deleteIcon} onClick={ props.onRemoveButtonClick }/>
     </div>
   );
 }

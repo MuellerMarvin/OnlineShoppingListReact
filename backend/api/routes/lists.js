ea417a9ad768dd2e24f9ffa1e75c0c54aa97f1c4
MuellerMarvin <marvin.mueller@code.berlin>
2019-10-21 17:15:03 +0200

    API now provides a default list

--- backend/api/routes/lists.js
+++ backend/api/routes/lists.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+var list = [ 'apples', 'bananas', 'cucumbers', 'coconuts', 'oranges', 'pineapples', 'lemons' ];
+
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Handling GET requests to /lists'
+        list: list,
     });
 });
 
 router.post('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Handling POST requests to /lists'
+        message: 'Handling POST requests to /lists',
     });
 });
+
+router.patch(''), (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling PATCH requests to /lists',
+    });
+}
 
 module.exports = router;

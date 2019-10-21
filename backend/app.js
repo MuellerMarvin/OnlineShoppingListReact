059992d2e1b275335eb654b3e87ec49b4d1b3ff6
MuellerMarvin <marvin.mueller@code.berlin>
2019-10-21 19:08:32 +0200

    Added default headers to express backend

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,13 @@ const app = express();
 
 const listRoutes = require('./api/routes/lists');
 
+// general config for all routes
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+// routes
 app.use('/lists', listRoutes);
 
 module.exports = app;

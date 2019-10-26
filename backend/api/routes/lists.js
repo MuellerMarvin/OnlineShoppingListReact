2b50a58e88ab4a49a98feca8afde506c639a1868
MuellerMarvin <marvin.mueller@code.berlin>
2019-10-27 01:16:11 +0200

    Making logs more useful and clean

--- backend/api/routes/lists.js
+++ backend/api/routes/lists.js
@@ -4,14 +4,18 @@ const router = express.Router();
 var list = [ 'apples', 'cucumbers', 'coconuts', 'oranges', 'pineapples', 'lemons' ];
 
 router.get('/', (req, res, next) => {
-    console.log("Received GET request on '/lists'")
+    // Write API-request to log
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log("[" + (new Date()).toUTCString() + " | " + ip + "] GET /lists");
     res.status(200).json({
         list: list,
     });
 });
 
 router.post('/', (req, res, next) => {
-    console.log("Received POST request on '/lists'");
+    // Write API-request to log
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log("[" + (new Date()).toUTCString() + " | " + ip + "] POST /lists -> " + (req.body.list.toString()));
     list = req.body.list;
     res.status(200).json({
         list: list,
